d4cdd3316c3fc3238971560227516ad874f3b0ee
genecywang <wanggiyans@gmail.com>
2024-05-19 02:53:31 +0800

    chore: format code using formatter tool

--- socat_server.sh
+++ socat_server.sh
@@ -4,20 +4,20 @@ PORT=80
 SOCAT_PID=""
 
 cleanup() {
-    echo "Cleaning up..."
-    kill -SIGTERM "${SOCAT_PID}"
-    wait "${SOCAT_PID}"
-    exit
+  echo "Cleaning up..."
+  kill -SIGTERM "${SOCAT_PID}"
+  wait "${SOCAT_PID}"
+  exit
 }
 
 trap cleanup INT TERM
 
 web() {
-    local port="${PORT}"
-    socat \
-        -v -d -d \
-        TCP-LISTEN:${port},crlf,reuseaddr,fork \
-        SYSTEM:"
+  local port="${PORT}"
+  socat \
+    -v -d -d \
+    TCP-LISTEN:${port},crlf,reuseaddr,fork \
+    SYSTEM:"
             echo HTTP/1.1 200 OK;
             echo Content-Type\: text/plain;
             echo;
@@ -26,15 +26,15 @@ web() {
             echo \"Server: \$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\";
             echo \"Client: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\";
         " &
-    SOCAT_PID=$!
-    wait "${SOCAT_PID}"
+  SOCAT_PID=$!
+  wait "${SOCAT_PID}"
 }
 
 case ${1} in
-  start)
-    web
-    ;;
-  *)
-    exec "$@"
-    ;;
+start)
+  web
+  ;;
+*)
+  exec "$@"
+  ;;
 esac
